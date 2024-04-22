794fad0a91ef42354969724615e83faa109a9daa
corsa144 <corsa144@gmail.com>
2024-04-22 18:59:46 -0300

    Ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clase_1';
+  public title = 'clase_1';
+  public edad_uno = 0
+  public edad_dos = 0
+  public suma = 0
+  public promedio = 0
+
+  constructor(){
+
+  }
+
+  ngOnInit():void{
+  }
+
+  calcular(){
+    this.suma = this.edad_uno + this.edad_dos
+    this.promedio = this.suma / 2
+  }
+
+  limpiar(){
+    this.edad_uno = 0
+    this.edad_dos = 0
+    this.suma = 0
+    this.promedio = 0
+  }
 }
